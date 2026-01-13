d6db4c79e195f16b37399163d15c218bd88778a2
Chandunandu47 <chandrashekar471997@gmail.com>
2026-01-13 15:11:16 +0530

    Add files via upload

---
+++ ziva-website/script.js
@@ -0,0 +1,24 @@
+.location {
+    padding: 60px 20px;
+    text-align: center;
+    background: #f8f8f8;
+}
+
+.location h2 {
+    color: #1f5132;
+    margin-bottom: 15px;
+}
+
+.location p {
+    font-size: 16px;
+    margin-bottom: 20px;
+}
+
+.location iframe {
+    width: 90%;
+    max-width: 800px;
+    height: 350px;
+    border: 0;
+    border-radius: 12px;
+}
+
